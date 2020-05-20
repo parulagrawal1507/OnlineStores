57b625659abf08326fbeb08e54414519a3bd7cbe
p0a00or <Parul.Agrawal@walmartlabs.com>
2020-05-20 18:02:21 +0530

    Updates

---
+++ precache-manifest.46182660b3c4ff17a4970330be3a2eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd445e82c35d191df5743b01167c64f7",
+    "url": "/OnlineStores/index.html"
+  },
+  {
+    "revision": "36f9c9191f0e04cab2f6",
+    "url": "/OnlineStores/static/css/2.ca79aa8d.chunk.css"
+  },
+  {
+    "revision": "c5a4084ddad6daa1acf4",
+    "url": "/OnlineStores/static/css/main.b7a31cff.chunk.css"
+  },
+  {
+    "revision": "36f9c9191f0e04cab2f6",
+    "url": "/OnlineStores/static/js/2.13c567b6.chunk.js"
+  },
+  {
+    "revision": "98e1fa34c20870296bcdcbfc49da9541",
+    "url": "/OnlineStores/static/js/2.13c567b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a4084ddad6daa1acf4",
+    "url": "/OnlineStores/static/js/main.ff9128ce.chunk.js"
+  },
+  {
+    "revision": "b646bf972a54892c3a8f",
+    "url": "/OnlineStores/static/js/runtime-main.222135e4.js"
+  }
+]);
